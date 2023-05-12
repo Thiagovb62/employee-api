ad14c046574c46f5358f7467ca8299f530ccf419
thiago.bezerra <thiago.bezerra@dataprev.gov.br>
2023-05-11 22:19:34 -0300

    craindo a navbar do projeto com tailwind

---
+++ frontend/src/components/AddEmployee.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AddEmployee = () => {
+    return (
+        <div className="flex max-w-2xl mx-auto shadow border-b">
+            <div className="px-8 py-8">
+                <h1 className="font-thin text-2xl text-wider ">Add New Employee</h1>
+            </div>
+            <div className="items-center justify-center h-14 w-full">
+            </div>
+        </div>
+    )
+}
+export default AddEmployee;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,17 @@
+// crie uma navBar bonita usando tailwindcss
+
+import React from 'react'
+
+const Navbar = () => {
+
+    return (
+        <div className="bg-gray-800">
+            <div className="h-16 flex items-center">
+                <p className="text-amber-50 px-8 font-bold">Employer Management System</p>
+            </div>
+        </div>
+    )
+
+}
+
+export default Navbar
